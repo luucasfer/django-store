1d78e128175ff7d2465494e6d12504e76ea04b71
Lucas <lucas.ferreira@blueshift.com.br>
2023-06-21 07:58:38 -0300

    21062023

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', status=200, context={
+    return render(request, 'recipes/pages/home.html', status=200, context={
         'name': 'Teste'
     })
-
-def about(request):
-    return render(request, 'recipes/contato.html')
-
-def contact(request):
-    return HttpResponse("CONTACT")
